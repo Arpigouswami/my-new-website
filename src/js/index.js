97a43531582c6c23d12b00da18b8415f25128c0a
Arpita <arpitagouswami@gmail.com>
2021-10-24 21:44:54 +0100

    update file

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import initTilt from "./tilt";
 import initSr from "./sr";
 import { addResume } from "./utils";
-import resume from "../assets/resume";
+import resume from "../assets/resume.pdf";
 
 export default function initApp() {
   initSr();
